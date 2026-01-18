d6f872430fa54a93c1387c335e1ed972181b8644
Praveen Kumar <singhsatyam312005@gmail.com>
2026-01-18 08:19:42 +0530

    first commits

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://worksathi-frontend.vercel.app"
+      "https://worksathi.vercel.app/"
     ],
     credentials: true
   })
